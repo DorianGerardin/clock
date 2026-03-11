fe85e487d50cae42170bbcca3ac3aca64c558717
Dorian Gerardin <dorian.gerardin@gmail.com>
2026-03-11 11:14:42 +0100

    PWA v1

---
+++ img/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "digiclock-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./clock.js",
+  "./img/clockIcon_192.png",
+  "./img/clockIcon_512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
